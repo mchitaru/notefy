1a409d9ec7c793a55706f807adcddcb654907db6
marius.chitaru <marius.chitaru@gmail.com>
2023-10-13 14:53:05 +0300

    Add schema & navbar items

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { ConvexClientProvider } from "@/components/providers/convex-provider"
@@ -42,6 +43,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="notefy-theme"
           >
+            <Toaster  position="bottom-center" />
             {children}
           </ThemeProvider>          
         </ConvexClientProvider>
